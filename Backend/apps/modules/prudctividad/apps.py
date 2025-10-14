2ecb034b4275beb0b509ec95a43438071fe8e3f8
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2025-10-13 22:30:07 -0500

    añadi el modulo de productividad

--- Backend/apps/modules/prudctividad/apps.py
+++ Backend/apps/modules/prudctividad/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DrivingConfig(AppConfig):
+class PrudctividadConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "driving"
+    name = "prudctividad"
